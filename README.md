9e76bf7208a42837759271e98a15896ec19076e6
android-47 <jx.garcia@outlook.com>
2021-03-24 16:05:38 -0700

    Added node_modules, rearranged main function and updated the prompt function.

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# password_generator
+# Password Generator
+
+By Javier Garcia Ramirez (android-47)
+Wed. Mar 24, 2021
+
+## GAMEPLAY
+
+### DESCRIPTION
+
+### STEPS ON HOW TO COMPILE AND RUN THE PROGRAM
+1. Compile the program using C++ 11 by entering the following into the terminal command line  
+
+```
+g++ -std=c++11 connect_four.cpp
+```
+
+2. Run the program by running the executable file along with command line arguments in the following order: number of 
+   players, number of rows, number of columns. Players can be either 1 or 2, rows and columns have to be between 1-20. 
+   For example, if the following line is entered into the terminal command line, it will be a 2 player game with 15 rows and 20 columns  
+
+```
+./a.out 2 15 20
+```
--- password_generator.js
+++ password_generator.js
@@ -1,45 +1,33 @@
 // author: Javier Garcia Ramirez
-// create date: Thursday, December 24, 2020
-// last modified: Friday, December 25, 2020
+// create date: R. Dec 24, 2020
+// last modified: W. Mar 24, 2021
 // filename: password-generator.js
 // description: 
 // runtime: 
-// creation time: N/A
 
 
 const prompt = require('prompt-sync')({sigint: true});
 
+/*
+*/
 
-
-function promptUser() {
-    const promptsList = ["Enter password length: ", "Enter how many upper case: ", 
-    "Enter how many lower case: ", "Enter how many numbers: ", "Enter how many special characters: "];
-
-    const testList = [];
-    let sum = 0;
-    for (x = 1; x < testList.length; x++) { // x = 1 so the length isnt added
-        sum += testList[x]; 
-    }
-
-    let userInput = " ";
-    for (i in promptsList){
-        let error = true;
-        while (error == true) {
-            userInput = Number(prompt(promptsList[i]));
-            error = checkInput(userInput, i);  // i = 0 - 4
-            if (i != 0 && testList[0] < sum + userInput){
-                console.log ("amount of upper, lower, numbers, special character ammount must not be grater to password length");
-                error = true;
-            } else if (i != 0 && testList[0] < userInput){
-                console.log ("input cannot be more than password length");
-                error = true;
-            }
+function promptUnsignedInt(prompt_mssg, error_mssg, min_val, max_val) { // can't handle empty
+    let test_int = -2;
+    let test_str = "";
+    do {
+        test_int == -2 ? test_str = prompt(prompt_mssg) : test_str = prompt(error_mssg + prompt_mssg);
+        if (test_str.length == 0)
+            test_str = "-1";
+        for (let i = 0; i < test_str.length; i++) {
+            if (!('0' <= test_str[i] && test_str[i] <= '9'))
+                test_str = "-1"; // display error
         }
-        testList.push(userInput); // check if first
-    }
-    return testList;
-}
+        test_int = Number(test_str);
+    } while (test_int < min_val || max_val < test_int);
 
+    return test_int;
+}
+/*
 
 function checkInput(userInput, scenerio, testList) { // notify error or explain error
     const TWOList = ["password length must greater than 0", 
@@ -61,20 +49,29 @@ function checkInput(userInput, scenerio, testList) { // notify error or explain 
     return error;
 }
 
+*/
 
 
 function createPassword(length, upper, lower, number, special) {
     const password = [];
-    const typeList = [["Upper cases: "],["Lower cases: "],["Numbers: "],["Special characters: "]];
+    const typeList = [["\nUpper: "],["Lower: "],["Number: "],["Special: "]];
     const requirments = [upper, lower, number, special];
+
+
+
     for (i = 0; i <= length-1; i++) { // include length or length-1?
         random_number = selectType(requirments, length);
+
+
+
         askii_code = getChar(random_number);
         password_char = String.fromCharCode(askii_code); // convert to askii value
         password.push(password_char);
         typeList[random_number].push(password_char);
     }
-    for (i = 0; i <= 3; i++){ // 3 represents length of typeList
+
+
+    for (i = 0; i < 4; i++){ //  printa typelist
         console.log(typeList[i][0], typeList[i].slice(1));
     } 
     return password;
@@ -93,14 +90,15 @@ function selectType(list_of_req, length) {
     
     return type;
 }
-function getChar(num){
+
+function getChar(num){ // depending on the type/case, finds the askii range and returns a random number between that range
     var range = [];
-    switch (num) {
-        case 0: range.push(90, 65);
-        case 1: range.push(122, 97);// ascii for lowercase 
-        case 2: range.push(57, 48);
-        default:
-        range = test2(range);
+    switch (num) { // bigger num on left side
+        case 0: range.push(90, 65);  // ascii range for upper case letters
+        case 1: range.push(122, 97); // ascii for lowercase letters
+        case 2: range.push(57, 48); // ascii range for numbers 0-9
+        default: // for special characters
+        range = test2(range); // this should be pushing two things too, passing in entire array an updating it in that function
     }
     max = range[0];
     min = range[1];
@@ -112,24 +110,41 @@ function getChar(num){
 function test2(range){
     x = Math.floor((Math.random() * 4) + 1);
     switch(x) {
-        case 1: range.push(47, 33);
-        case 2: range.push(64, 58);
-        case 3: range.push(96, 91);
-        default: range.push(126, 123);
+        case 1: range.push(47, 33);    // special chars set 1
+        case 2: range.push(64, 58);    // special chars set 2
+        case 3: range.push(96, 91);    // special chars set 3
+        default: range.push(126, 123); // special chars set 4
     }
     return range;
 }
 
-
 function main() {
-    let password_requirements = promptUser();
-    console.log (password_requirements);
-    p_length = Number(password_requirements[0]);
-    p_upper = Number(password_requirements[1]);
-    p_lower = Number(password_requirements[2]);
-    p_number = Number(password_requirements[3]);
-    p_special = Number(password_requirements[4]);
-    password = createPassword(p_length, p_upper, p_lower, p_number, p_special);
+    const promptsList = [
+    "Enter password length: ", 
+    "Enter how many upper case letters: ", 
+    "Enter how many lower case letters: ", 
+    "Enter how many numbers: ", 
+    "Enter how many special characters: "];
+    const requirements = [0, 0, 0, 0, 0];
+    requirements[0] = promptUnsignedInt(promptsList[0], "ERROR! ", 0, 100);
+    let copy_length = requirements[0];
+
+    for (let i = 1; i < 5; i++) { // special chars should be calculated by itself // 5 is promptsList length, i = 1 so length is not changed
+        // if i == 4
+        if (copy_length != 0) {
+            requirements[i] = promptUnsignedInt(promptsList[i], "ERROR! ", 0, copy_length);
+            copy_length -= requirements[i];
+        } else {
+            requirements[i] = 0; 
+        }
+    }
+    let length = requirements[0];
+    let upper = requirements[1];
+    let lower = requirements[2];
+    let number = requirements[3];
+    let special = requirements[4];
+    // would you like to choose char to include/? how many? prompt that many times
+    let password = createPassword(length, upper, lower, number, special);
     console.log ("\nPASSWORD:", password.join(""), "\n");
 }
 
