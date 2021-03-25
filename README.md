2132fbca3c17d9b4b8c31e5715252cb2dad1ad8a
android-47 <jx.garcia@outlook.com>
2021-03-25 00:01:23 -0700

    Added quick password feature and updated README.md.

--- README.md
+++ README.md
@@ -3,23 +3,30 @@
 By Javier Garcia Ramirez (android-47)
 Wed. Mar 24, 2021
 
-## GAMEPLAY
+FEATURES
+* User can select to generate a quick password or a password where the components are custumizable
+* User can select password components including length (up to 100), number of upper/lower case letters, numbers, and special characters
+* Random characters are randomly dispersed throughout the password
+* Error handles all bad input by displaying error message and prompting the user again
+* Automatically calculates the number of special characters since the length is given
+* Displays password individual components as a group at the end
+* Random characters are randomly dispersed through the password
 
-### ALGORITHM
+## GAMEPLAY
+### Quick 16 Character Password
+![Quick 16 Character Password](./images/quick_password.png "Test")
+### Custom Password
+![Custom Password](./images/custom_password.png "Test")
 
 ### DESCRIPTION
+This program that allows the user to generate custom complex passwords. Random characters are randomly dispersed throughout the password, meaning all the lower case letters are not bunched up or the numbers are not bunched up, etc. The user is able to choose between a quick password, where the components include 16 characters in length including 4 upper/lower case letters, 4 numbers, and 4 special characters, OR the user can decide to generate a custom password. The custom password allows the user to decide all components such as the on the length, up to 100, and how many upper/lower case letters, numbers, and special characters. At the end, the program displays the password as well as the individual components to show the user the password generated satisfies all the requirements.
 
-### STEPS ON HOW TO COMPILE AND RUN THE PROGRAM
-1. Compile the program using C++ 11 by entering the following into the terminal command line  
 
-```
-g++ -std=c++11 connect_four.cpp
-```
+### STEPS ON HOW TO RUN THE PROGRAM
+1. Make you have the node_modules folder included from this repository in order to run the program using JavaScript (ES6).
+2. Enter the following line on the terminal command line:    
 
-2. Run the program by running the executable file along with command line arguments in the following order: number of 
-   players, number of rows, number of columns. Players can be either 1 or 2, rows and columns have to be between 1-20. 
-   For example, if the following line is entered into the terminal command line, it will be a 2 player game with 15 rows and 20 columns  
 
 ```
-./a.out 2 15 20
+node password_generator.js
 ```
--- password_generator.js
+++ password_generator.js
@@ -2,16 +2,23 @@
 // create date: R. Dec 24, 2020
 // last modified: W. Mar 24, 2021
 // filename: password_generator.js
-// description: 
-// runtime: 
+// description: generates a random password
+// runtime: n/a
+
+
+/* Upcoming features: 
+
+  * in error message, tell user the exact issue and how to solve
+  * in custom mode, ask user if they want to inculde a specific character
+  * in custom mode, ask user if they want to allow repeated characters
+  * ask user how many passwords to generate in the beginning (1-10)
+  * in the end ask the user if the want to generate more password(s) or quit
+  * add runtime
+
+*/
 
 
 const prompt = require('prompt-sync')({sigint: true}); // needed to prompt the user
-// quick password
-// customize
-// advanced customize
-/*
-*/
 
 function promptUnsignedInt(prompt_mssg, error_mssg, min_val, max_val) { // can't handle empty
     let test_int = -2;
@@ -30,44 +37,21 @@ function promptUnsignedInt(prompt_mssg, error_mssg, min_val, max_val) { // can't
     return test_int;
 }
 
-/*
-
-function checkInput(userInput, scenerio, testList) { // notify error or explain error
-    const TWOList = ["password length must greater than 0", 
-    "cannot be a negative number.", "must enter a whole number"];
-    let error = true;
-    switch (error) {
-        case Number.isInteger(userInput) == false || userInput == " ":
-            console.log("Error:", TWOList[2]);
-            break;
-        case userInput < 1 && scenerio == 0:
-            console.log("Error:", TWOList[0]);
-            break;
-        case userInput < 0 && scenerio != 0:
-            console.log("Error:", TWOList[1]);
-            break;
-        default:
-            error = false; // error becomes false if no errors from above
-    }
-    return error;
-}
-
-*/
-
 
 function createPasswordAskii(length, upper, lower, number, special) {
     const password = [];
     const requirments = [upper, lower, number, special];
 
     for (let i = 0; i < length; i++) {
-        random_number = selectType(requirments, length); // type could be upper, lower, number, special
+        random_number = selectCharType(requirments, length); // type could be upper, lower, number, special
         askii_val = getChar(random_number); // gets askii vale between the range of the ...
         password.push(askii_val);
     }
     return password; // return askii values of password
 }
 
-function selectType(list_of_req, length) {
+
+function selectCharType(list_of_req, length) {
     validType = false;
     let type = 0;
     while (validType == false) {
@@ -80,14 +64,16 @@ function selectType(list_of_req, length) {
     return type;
 }
 
-function getChar(num){ // depending on the type/case, finds the askii range and returns a random number between that range
+
+// depending on the type/case, finds the askii range and returns a random number between that range
+function getChar(num){ 
     var range = [];
     switch (num) { // bigger num on left side
         case 0: range.push(90, 65);  // ascii range for upper case letters
         case 1: range.push(122, 97); // ascii for lowercase letters
         case 2: range.push(57, 48); // ascii range for numbers 0-9
         default: // for special characters
-        range = test2(range); // this should be pushing two things too, passing in entire array an updating it in that function
+        range = getSpecialChar(range); // this should be pushing two things too, passing in entire array an updating it in that function
     }
     max = range[0];
     min = range[1];
@@ -96,7 +82,7 @@ function getChar(num){ // depending on the type/case, finds the askii range and 
 }
 
 
-function test2(range){
+function getSpecialChar(range){
     x = Math.floor((Math.random() * 4) + 1);
     switch(x) {
         case 1: range.push(47, 33);    // special chars set 1
@@ -109,58 +95,71 @@ function test2(range){
 
 
 function getRequirements() {
-    const promptsList = [
-    "Enter password length: ", 
-    "Enter how many upper case letters: ", 
-    "Enter how many lower case letters: ", 
-    "Enter how many numbers: ", 
-    "Enter how many special characters: "];
+    let prompt = "Enter option number; quick password(1) custom password(2): ";
     const requirements = [0, 0, 0, 0, 0];
-    requirements[0] = promptUnsignedInt(promptsList[0], "ERROR! ", 0, 100);
-    let copy_length = requirements[0];
+    let password_option = promptUnsignedInt(prompt, "<ERROR> ", 1, 2);
 
-    for (let i = 1; i < 5; i++) { // special chars should be calculated by itself // 5 is promptsList length, i = 1 so length is not changed
-        // if i == 4
-        if (copy_length != 0) {
-            requirements[i] = promptUnsignedInt(promptsList[i], "ERROR! ", 0, copy_length);
-            copy_length -= requirements[i];
-        } else {
-            requirements[i] = 0; 
-        }
+    if (password_option == 1) {
+        requirements[0] = 16;
+        for (let i = 1; i < 5; i++)
+            requirements[i] = 4;
+    } else {
+        console.log("");
+        getCustomReq(requirements);
     }
     return requirements;
 }
 
+
+function getCustomReq(customReq) {
+    const prompts = ["Enter password length: ", 
+    "Enter how many upper case letters: ", "Enter how many lower case letters: ", 
+    "Enter how many numbers: ", "Enter how many special characters: "];
+
+    customReq[0] = promptUnsignedInt(prompts[0], "<ERROR> ", 1, 100);
+    let copy_length = customReq[0]; // replace with arrow function for sum
+
+    for (let i = 1; i < 5; i++) { // 5 is prompts length, i = 1 so length is not changed
+        if (i == 4) {
+            customReq[4] = copy_length;
+        } else if (copy_length != 0) {
+            customReq[i] = promptUnsignedInt(prompts[i], "<ERROR> ", 0, copy_length);
+            copy_length -= customReq[i];
+        } else
+            customReq[i] = 0; 
+    }
+    return customReq;
+}
+
+
 function print_password(passwordAskii) {
     const password = [];
-    let typeList = [["\nUpper: "],["Lower: "],["Number: "],["Special: "]]; // turn this to an object?
+    let typeList = { upper: [], lower: [], number: [], special: [] };
     
-    // convert to askii char
-    for (i of passwordAskii) {
-        askii_char = String.fromCharCode(i); 
-        typeList = insertToTypeList(askii_char, typeList);
-        password.push(askii_char);
+    // convert askii val to askii char
+    for (val of passwordAskii) {
+        char = String.fromCharCode(val); 
+        insertToTypeList(char, typeList);
+        password.push(char);
     }
 
-    //  print typelist
-    for (i of typeList) 
-        console.log(i[0], i.slice(1));
-
+    console.log("\nIndividual components", typeList);
     console.log ("\nPASSWORD:", password.join(""), "\n");
 }
 
+
 function insertToTypeList(askii_char, typeList) {
-        // add char to a list of it's type
-        if ('A' <= askii_char && askii_char <= 'Z')
-            typeList[0].push(askii_char);
-        else if ('a' <= askii_char && askii_char <= 'z')
-            typeList[1].push(askii_char);
-        else if ('0' <= askii_char && askii_char <= '9')
-            typeList[2].push(askii_char);
-        else
-            typeList[3].push(askii_char);
-        return typeList;
+    // add char to a list of it's type
+    if ('A' <= askii_char && askii_char <= 'Z')      // is askii upper
+        typeList.upper.push(askii_char);
+    else if ('a' <= askii_char && askii_char <= 'z') // is askii lower
+        typeList.lower.push(askii_char);
+    else if ('0' <= askii_char && askii_char <= '9') // is askii a num
+        typeList.number.push(askii_char);
+    else                                             // askii is special
+        typeList.special.push(askii_char);
 }
+
 
 function main() {
     let req = getRequirements();
@@ -169,11 +168,9 @@ function main() {
     let lower = req[2];
     let number = req[3];
     let special = req[4];
-    // would you like to choose char to include/? how many? prompt that many times, repeat chars?
-
     let passwordAskii = createPasswordAskii(length, upper, lower, number, special);
     print_password(passwordAskii);
-    // print req
 }
+
 
 main()
